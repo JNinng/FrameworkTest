ca67ee28a7435a109b8a7f62b716afaac0fc695d
JNinng <JNinng@163.com>
2024-02-29 11:28:12 +0800

    feat(all): 使用 mybatis 访问数据库（注解形式）

--- src/main/java/top/ninng/dao/IStudentDao.java
+++ src/main/java/top/ninng/dao/IStudentDao.java
@@ -1,10 +1,12 @@
 package top.ninng.dao;
 
+import org.apache.ibatis.annotations.Select;
 import top.ninng.domain.Student;
 
 import java.util.List;
 
 public interface IStudentDao {
 
+    @Select("select * from student")
     List<Student> selectAll();
 }
