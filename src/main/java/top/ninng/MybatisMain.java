35c5ec12b12810e572b48e17de2446c52af3da00
JNinng <jninng@163.com>
2024-03-01 15:58:05 +0800

    feat(student): 增删查改
    * 增加增删查改
    * domain 对象属性名与数据库列名映射

--- src/main/java/top/ninng/MybatisMain.java
+++ src/main/java/top/ninng/MybatisMain.java
@@ -23,14 +23,32 @@ public class MybatisMain {
             SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
             SqlSessionFactory factory = sqlSessionFactoryBuilder.build(inputStream);
             // 生产 session
-            SqlSession sqlSession = factory.openSession();
+            SqlSession sqlSession = factory.openSession(true);
             // 创建 dao 代理对象
             IStudentDao studentDao = sqlSession.getMapper(IStudentDao.class);
+
             // 执行
+            // 插入新的
+            Student student = new Student("+", "-", String.valueOf(System.currentTimeMillis()));
+            studentDao.insertStudent(student);
+            System.out.println("insert: " + student);
+
+            // 条件查询
+            student = studentDao.selectByAddress(student.getAddress());
+            System.out.println("select: " + student);
+
+            // 更新
+            student.setAddress(String.valueOf(System.currentTimeMillis()));
+            studentDao.updateStudent(student);
+
+            // 查询所有
             List<Student> students = studentDao.selectAll();
-            for (Student student : students) {
-                System.out.println(student);
+            for (Student s : students) {
+                System.out.println(s);
             }
+
+            // 删除
+            studentDao.deleteById(student.getId());
             sqlSession.close();
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/top/ninng/dao/IStudentDao.java
+++ src/main/java/top/ninng/dao/IStudentDao.java
@@ -1,12 +1,58 @@
 package top.ninng.dao;
 
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import top.ninng.domain.Student;
 
 import java.util.List;
 
 public interface IStudentDao {
 
+    /**
+     * 根据地址删除学生信息
+     *
+     * @param id
+     * @return
+     */
+    @Delete("delete from student where id=#{id}")
+    void deleteById(Integer id);
+
+    /**
+     * 插入一名学生信息
+     *
+     * @param student
+     */
+    @Insert("insert into student(name,sex,address) values(#{name},#{sex},#{address})")
+    void insertStudent(Student student);
+
+    /**
+     * 查询所有
+     *
+     * @return
+     */
     @Select("select * from student")
+    @Results(id = "studentMap", value = {
+            @Result(id = true, column = "id", property = "id"),
+            @Result(column = "name", property = "name"),
+            @Result(column = "sex", property = "sex"),
+            @Result(column = "address", property = "address")
+    })
     List<Student> selectAll();
+
+    /**
+     * 根据地址查询学生信息
+     *
+     * @param address
+     * @return
+     */
+    @Select("select * from student where address=#{address}")
+    @ResultMap(value = "studentMap")
+    Student selectByAddress(String address);
+
+    /**
+     * 更新学生信息
+     *
+     * @param student
+     */
+    @Update("update student set name=#{name},sex=#{sex},address=#{address} where id=#{id}")
+    void updateStudent(Student student);
 }
