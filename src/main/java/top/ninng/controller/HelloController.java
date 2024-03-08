37ee2159f07c865e91e6a9d7c42de2826507b851
JNinng <jninng@163.com>
2024-03-08 13:09:18 +0800

    feat(hello): 文件上传

--- src/main/java/top/ninng/controller/HelloController.java
+++ src/main/java/top/ninng/controller/HelloController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import top.ninng.domain.Hello;
 import top.ninng.service.IHelloService;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * Hello 控制器类
@@ -28,6 +33,9 @@ public class HelloController {
      */
     @RequestMapping(value = "/{name}", method = {RequestMethod.GET})
     public String hello(@PathVariable(value = "name") String name) {
+        if (name.equals("file")) {
+            return "file";
+        }
         return helloService.hello(name);
     }
 
@@ -51,5 +59,22 @@ public class HelloController {
     @RequestMapping(value = "/hello", method = {RequestMethod.POST})
     public String helloPostHello(Hello hello) {
         return helloService.hello("post .. " + hello);
+    }
+
+    /**
+     * 文件上传
+     *
+     * @return
+     */
+    @RequestMapping(value = "/upload", method = {RequestMethod.POST})
+    public String upload(HttpServletRequest request, MultipartFile upload) throws IOException {
+        String path = request.getSession().getServletContext().getRealPath("/upload");
+        File file = new File(path);
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        String fileName = UUID.randomUUID() + "_" + upload.getOriginalFilename();
+        upload.transferTo(new File(path, fileName));
+        return "file";
     }
 }
